3a665c91eff29f825521e64e726adea90964f149
Tushar-chaurasiya <164353732+Tushar-chaurasiya@users.noreply.github.com>
2024-07-01 13:59:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
 
 /*-----------------------------typed js-----------------------------------*/
 // const typed = new Typed('.multiple-text', {
-//     strings: ['Frontend Developer', 'Web Designer', 'Youtuber'],
+//     strings: ['Frontend Developer', 'Web Designer', 'Data Analysis'],
 //     typeSpeed: 70,
 //     backSpeed: 70,
 //     backDelay: 1000,
